622882065f61d3c78ed5daabd353d5bf6340a976
Marilainny <jkjrodrigues@hotmail.com>
2023-03-04 16:57:30 -0300

    url dinamica

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,8 @@ from .views import post_list, post_create, post_detail, post_update, post_delete
 
 urlpatterns = [
     path('', post_list),
-    path('detail', post_detail),
+    path('detail/<int:id>/', post_detail),
     path('create', post_create),
-    path('update', post_update),
+    path('update/<int:id>/', post_update),
     path('delete', post_delete),
 ]
