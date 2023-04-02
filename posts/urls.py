89847906f8a7678bf68c128bc342129fc6c36651
Marilainny <jkjrodrigues@hotmail.com>
2023-04-02 16:32:38 -0300

    delete

--- posts/urls.py
+++ posts/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import post_list, post_create, post_detail, post_update, post_delete
 
 urlpatterns = [
-    path('', post_list),
+    path('', post_list, name='list'),
     path('create/', post_create),
     path('<int:id>/edit/', post_update, name='update'),
-    path('delete/<int:id>/', post_delete),
+    path('<int:id>/delete/', post_delete),
     path('<int:id>/', post_detail, name='detail'),
 
 ]
